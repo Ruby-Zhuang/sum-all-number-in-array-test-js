f5c3212d5410600e009c1d06f620abd9f09f62fc
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-02 13:52:08 -0400

    Function to recursively sum all the numbers in an array even if it has nested arrays.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let sum = 0;
+
+  for (const num of array) {
+    if (!Array.isArray(num)) {
+      sum += num;
+    } else {
+      sum += sumItems(num);
+    }
+  }
+  return sum;
+};
 
 module.exports = sumItems;
